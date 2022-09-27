a8e25b74e56d0da144d3d82c79f05fa1f879f813
Jumpydoll-Test <brian@jumpydoll.com>
2022-09-27 03:40:05 +0000

    Initial commit

---
+++ src/main/java/com/jumpydoll/app/jumpydolltest/java/JavaApplication.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.app.jumpydolltest.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumpydoll/app/jumpydolltest/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.app.jumpydolltest.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
